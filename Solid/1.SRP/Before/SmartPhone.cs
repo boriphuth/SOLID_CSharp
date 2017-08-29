2e9e8bcd2e5a8d7bd88b3758b12ecc7eb39346fd
Boriphuth Saensukphattraka <boriphuth.saensukphattraka@live.com>
2017-08-29 22:04:02 +0700

    First commit.

---
+++ Solid/1.SRP/Before/SmartPhone.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Solid._1.SRP.Before
+{
+    public class SmartPhone
+    {
+        private List<string> apps;
+        private List<string> contacts;
+        private string phoneNumber;
+        private string phoneModel;
+        private int screenWidth;
+        private int screenHigth;
+
+        public void Call(int telNumber)
+        {
+        }
+
+        public void Ring()
+        {
+        }
+
+        public void InstallApp(string appName)
+        {
+            this.apps.Add(appName);
+        }
+
+        public void LaunchApp(string appName)
+        {
+            var app = this.apps.SingleOrDefault(a => a.Contains(appName));
+        }
+
+        public void AddContact(string contact)
+        {
+            this.contacts.Add(contact);
+        }
+
+        public int GetScreenPixel()
+        {
+            return this.screenWidth * this.screenHigth;
+        }
+
+        public string GetModel()
+        {
+            return this.phoneModel;
+        }
+    }
+}
